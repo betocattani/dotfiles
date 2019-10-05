2353f9dc381f3c1cec235a4397a328f85747cf31
betocattani <luiz.cattani@youse.com.br>
2019-10-05 13:48:22 -0300

    create setup file

---
+++ setup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+OS=$(uname -s)
+
+git clone https://github.com/betocattani/dotfiles.git ~/.dotfiles
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+if [ "$OS" == "Darwin" ]; then
+  sudo softwareupdate -ia
+  
+  xcode-select --install
+  
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  
+  export PATH=/usr/local/bin:$PATH
+  
+  pushd ~/.dotfiles
+  brew tap Homebrew/bundle
+  brew install caskroom/cask/brew-cask
+  brew bundle
+  popd
+elif [ "$OS" == "Linux" ]; then
+  sudo apt install -y ruby
+  
+  git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+  
+  cd ~/.rbenv && src/configure && make -C src
+  
+  git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+fi
+
+~/.dotfiles/install.sh
